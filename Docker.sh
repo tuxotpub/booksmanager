416a4d21d97093f40453496ccdd5c0f2d644cfcc
tuxsamo <tuxotpub@yahoo.com>
2018-12-01 02:01:04 +0100

    rules for validations

--- Docker.sh
+++ Docker.sh
@@ -1,1 +1,5 @@
-docker run \ --name booksmanagement \ -p 8088:8088 \ -d \ tuxotpub/booksmanagement:latest
+docker run \
+    --name booksmanagement \
+    -p 8088:8088 \
+    -d \
+    tuxotpub/booksmanagement:latest
